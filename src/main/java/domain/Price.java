62ebdaef228e9da8db56d3bea7ecbed10d8f44a8
thelyron <markus.sickor@gmail.com>
2014-04-24 14:47:14 +0200

    Fixed validFrom

--- src/main/java/domain/Price.java
+++ src/main/java/domain/Price.java
@@ -29,8 +29,7 @@ public class Price
 		try
 		{
 			obj.put("priceList", priceList.getUuid());
-			obj.put("validFrom", 1398290400000l);
-			//obj.put("validFrom", inputDf.format(validFrom));
+			obj.put("validFrom", inputDf.format(validFrom));
 			obj.put("value", value);
 			return obj;
 		}
