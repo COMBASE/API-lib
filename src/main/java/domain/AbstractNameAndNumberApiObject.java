0d47022f044b59c87f343aa56b06c417360ed522
thelyron <markus.sickor@gmail.com>
2014-12-01 13:47:28 +0100

    merge2

--- src/main/java/domain/AbstractNameAndNumberApiObject.java
+++ src/main/java/domain/AbstractNameAndNumberApiObject.java
@@ -1,29 +1,51 @@
 package domain;
 
-import domain.interfaces.HasName;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
-public abstract class AbstractNameAndNumberApiObject extends AbstractNumberApiObject implements
-	HasName
+import domain.interfaces.HasName;
+import domain.interfaces.HasNumber;
+
+public abstract class AbstractNameAndNumberApiObject<T extends HasName & HasNumber & HasId> extends
+	AbstractNumberApiObject<T> implements HasName
 {
+
+	public static abstract class Builder extends Init<Builder>
+	{
+
+		@Override
+		public abstract AbstractNameAndNumberApiObject<?> build();
+
+		@Override
+		protected Builder self()
+		{
+			return this;
+		}
+
+	}
+
+	protected static abstract class Init<T extends Init<T>> extends AbstractNumberApiObject.Init<T>
+	{
+		private String name;
+
+		@Override
+		public abstract AbstractNameAndNumberApiObject<?> build();
+
+		public T name(final String value)
+		{
+			name = value;
+			return self();
+		}
+	}
+
 	private static final long serialVersionUID = 4379932372164338249L;
 
 	private String name;
 
-	public AbstractNameAndNumberApiObject(final ApiNameAndNumberObjectBuilder builder)
+	public AbstractNameAndNumberApiObject(final Init<?> init)
 	{
-		super(builder);
-		this.name = builder.name;
-	}
-
-	public static abstract class ApiNameAndNumberObjectBuilder extends ApiNumberObjectBuilder
-	{
-		private String name;
-
-		public ApiNameAndNumberObjectBuilder name(final String value)
-		{
-			name = value;
-			return this;
-		}
+		super(init);
+		this.name = init.name;
 	}
 
 	@Override
@@ -33,8 +55,42 @@ public abstract class AbstractNameAndNumberApiObject extends AbstractNumberApiOb
 	}
 
 	@Override
+	protected int hashCode(int result)
+	{
+		super.hashCode(result);
+
+		final int prime = 31;
+		result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+
+		return result;
+	}
+
+
+	@Override
 	public void setName(final String name)
 	{
 		this.name = name;
+	}
+
+	@Override
+	protected StringBuilder toString(final StringBuilder builder)
+	{
+		super.toString(builder);
+
+		if (name != null)
+		{
+			builder.append("_name=");
+			builder.append(this.name);
+		}
+		return builder;
+	}
+
+	@Override
+	public JSONObject appendJSON(JSONObject obj) throws JSONException
+	{
+		obj = super.appendJSON(obj);
+		obj.put("name", getName());
+
+		return obj;
 	}
 }
