feb51879006d12998f1aede3ac87ef5655c50d9f
thelyron <markus.sickor@gmail.com>
2014-12-01 13:48:10 +0100

    merge3

--- src/main/java/domain/AbstractNumberApiObject.java
+++ src/main/java/domain/AbstractNumberApiObject.java
@@ -1,28 +1,51 @@
 package domain;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 import domain.interfaces.HasNumber;
 
-public abstract class AbstractNumberApiObject extends AbstractApiObject implements HasNumber
+public abstract class AbstractNumberApiObject<T extends HasNumber & HasId> extends
+	AbstractApiObject<T> implements HasNumber
 {
+
+	public static abstract class Builder extends Init<Builder>
+	{
+
+		@Override
+		public abstract AbstractNumberApiObject<?> build();
+
+		@Override
+		protected Builder self()
+		{
+			return this;
+		}
+
+	}
+
+	protected static abstract class Init<T extends Init<T>> extends AbstractApiObject.Init<T>
+	{
+		private String number;
+
+		@Override
+		public abstract AbstractNumberApiObject<?> build();
+
+		public T number(final String value)
+		{
+			number = value;
+			return self();
+		}
+
+	}
+
 	private static final long serialVersionUID = -589391620327601920L;
 
 	private String number;
 
-	public AbstractNumberApiObject(final ApiNumberObjectBuilder builder)
+	public AbstractNumberApiObject(final Init<?> init)
 	{
-		super(builder);
-		this.number = builder.number;
-	}
-
-	public static abstract class ApiNumberObjectBuilder extends ApiObjectBuilder
-	{
-		private String number;
-
-		public ApiNumberObjectBuilder number(final String value)
-		{
-			number = value;
-			return this;
-		}
+		super(init);
+		this.number = init.number;
 	}
 
 	@Override
@@ -31,9 +54,42 @@ public abstract class AbstractNumberApiObject extends AbstractApiObject implemen
 		return this.number;
 	}
 
+
+	@Override
+	protected int hashCode(int result)
+	{
+		super.hashCode(result);
+
+		final int prime = 31;
+		result = prime * result + ((this.number == null) ? 0 : this.number.hashCode());
+
+		return result;
+	}
+
 	@Override
 	public void setNumber(final String number)
 	{
 		this.number = number;
+	}
+
+	@Override
+	protected StringBuilder toString(final StringBuilder builder)
+	{
+		super.toString(builder);
+		if (number != null)
+		{
+			builder.append("_nr=");
+			builder.append(this.number);
+		}
+		return builder;
+	}
+
+	@Override
+	public JSONObject appendJSON(JSONObject obj) throws JSONException
+	{
+		obj = super.appendJSON(obj);
+		obj.put("number", getNumber());
+
+		return obj;
 	}
 }
