84241a46edc09408b609edabd22900e40c48b34d
art <art@pc-art>
2015-01-06 10:42:13 +0100

    Loader for every existing object added

--- src/main/java/domain/ReferenceType.java
+++ src/main/java/domain/ReferenceType.java
@@ -3,7 +3,7 @@ package domain;
 public enum ReferenceType
 {
 	revision("updates"), uuid("id"), name("name"), number("number"), customerName("byName"), offset(
-		"updates"), code("code");
+		"updates"), code("code"), page("page");
 	private final String reference;
 
 	ReferenceType(final String s)
