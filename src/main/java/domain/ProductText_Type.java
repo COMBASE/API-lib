db85ba97278db96f568df4f29e8ca1ff8f537f5d
art <art@combase.de>
2015-01-13 13:29:37 +0100

    updated ProductText_Types

--- src/main/java/domain/ProductText_Type.java
+++ src/main/java/domain/ProductText_Type.java
@@ -4,7 +4,10 @@ package domain;
 public enum ProductText_Type
 {
 	description("ARTICLETEXT_DESCRIPTION"), nutritionals("ARTICLETEXT_NUTRITIONAL_VALUE"), ingredients(
-		"ARTICLETEXT_INGREDIENTS"), allergens("ARTICLETEXT_ALLERGENS"), receiptDescription("ARTICLETEXT_RECEIPT_TEXT");
+		"ARTICLETEXT_INGREDIENTS"), allergens("ARTICLETEXT_ALLERGENS"), receiptDescription("ARTICLETEXT_RECEIPT_TEXT"),
+		manual("ARTICLETEXT_MANUAL"), orderText("ARTICLETEXT_ORDER_TEXT"), careInstructions("ARTICLETEXT_CARE_INSTRUCTIONS"),
+		recipe("ARTICLETEXT_RECIPE"), certificate("ARTICLETEXT_CERTIFICATE"), warrantyText("ARTICLETEXT_WARRANTY_TEXT"),
+		infoUrl("ARTICLETEXT_INFO_URL");
 	private final String reference;
 
 	ProductText_Type(String s)
